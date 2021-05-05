29f67d81c81d34ebd4579f2b12d6ddb73c910266
apple <test@example.com>
2021-05-06 08:31:33 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root 'messages#index'
+
  namespace :messages do
  	resources :searches, only: :index, defaults: { format: :json }
  end
