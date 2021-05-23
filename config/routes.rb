dbb6895ee02280c61db129596bb029afea0989f4
apple <test@example.com>
2021-05-23 10:02:33 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   
+
+  get 'home/index'
+  get 'home/show'
+ devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root 'messages#index'
+ root to: "home#index"
 
  namespace :messages do
  	resources :searches, only: :index, defaults: { format: :json }
